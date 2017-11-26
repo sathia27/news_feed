ebd9730181d04620b0022a51902bd2ecf96ecf2d
Sathia <sathia2704@gmail.com>
2017-11-26 17:02:30 +0530

    pagination changes

--- app/controllers/news_feed_controller.rb
+++ app/controllers/news_feed_controller.rb
@@ -1,7 +1,7 @@
 class NewsFeedController < ApplicationController
   def index
     #Optimization: included users and comments to avoid lazy loading.
-    posts = Post.includes(:user, :comments).paginate(params[:page]||1)
+    posts = Post.includes(:user, :comments).paginate((params[:page]||1), (params[:limit] || 1))
     post_hash = posts.map { |a| PostPresenter.new(a).as_json }
     render :json => post_hash
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,7 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
  
-  def self.paginate(page_number=1)
-    limit = 50
+  def self.paginate(page_number, limit)
     offset = (page_number-1)*limit
     limit(limit).offset(offset)
   end
